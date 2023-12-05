f153be1ae1f0aedb0142240fb08df2b5fffe69ef
Redyf <mateusalvespereira7@gmail.com>
2023-12-05 15:39:35 -0300

    Tokyodark colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-  event = "VeryLazy",
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    event = "VeryLazy",
+  },
 
   -- Configure LazyVim to load the colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oxocarbon",
+      colorscheme = "tokyodark",
     },
   },
 }
